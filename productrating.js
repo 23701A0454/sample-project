c781a8a22b92953f3fce5bed99b1a621f4ee8782
23701A0454 <cgangababu2007@gmail.com>
2026-03-12 12:18:44 +0530

    Add files via upload

---
+++ productrating.js
@@ -0,0 +1,21 @@
+let rating = 3;
+switch(rating){
+    case 1:console.log("poor");break;
+    case 2:console.log("average");break;
+    case 3:console.log("good");break; 
+    default:console.log("invalid rating");
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
