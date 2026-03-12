f471fb2f8198270657db32ac576394ce4902426a
23701A0454 <cgangababu2007@gmail.com>
2026-03-12 12:18:04 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,8 @@
+let a=10,b=5;op="+";
+switch(op){
+    case"+":console.log("addition:",a+b);break;
+    case"-":console.log("subtraction:",a-b);break;
+    case"*":console.log("multiplication:",a*b);break;
+    case"/":console.log("division:",a/b);break;
+    default:console.log("invalid operator")
+}
