0ba8111edd12c1f533e58d7824a3c0a176a18f66
23701A0454 <cgangababu2007@gmail.com>
2026-03-12 12:19:56 +0530

    Add files via upload

---
+++ traffic.js
@@ -0,0 +1,7 @@
+let signal="green";
+switch(signal){
+    case"red":console.log("stop");break;
+    case"yellow":console.log("ready");break;
+    case"green":console.log("go");break;
+    default:console.log("invalid signal");
+}
