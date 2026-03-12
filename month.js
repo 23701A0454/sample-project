ce7c724be6498516f8c1c6a2fb33f3729c61f1b0
23701A0454 <cgangababu2007@gmail.com>
2026-03-12 12:19:26 +0530

    Add files via upload

---
+++ month.js
@@ -0,0 +1,8 @@
+let month = 3;
+switch(month){
+    case 1:console.log("jan");break;
+    case 2:console.log("feb");break;
+    case 3:console.log("mar");break;
+    case 4:console.log("apr");break;
+    default:console.log("other month");
+}
