a64a77117a8456c849db86121f24fe93791ba94a
23701A0454 <cgangababu2007@gmail.com>
2026-03-13 10:22:20 +0530

    Add files via upload

---
+++ evenorodd.js
@@ -0,0 +1,11 @@
+function evenOrOdd(num) {
+  if (num % 2 === 0) {
+    return "Even";
+} else {
+    return "Odd";
+}
+}
+
+// Example usage:
+console.log(evenOrOdd(4)); 
+console.log(evenOrOdd(7)); 
