104ce2c031de2147d0286d0de8a214e840b7575a
23701A0454 <cgangababu2007@gmail.com>
2026-03-13 11:04:46 +0530

    Add files via upload

---
+++ doubler.js
@@ -0,0 +1,9 @@
+
+function doublerGoal(goal) {
+  return goal * 2;
+}
+let myGoal = 50;
+let doubled = doublerGoal(myGoal);
+
+console.log("Original Goal:", myGoal);
+console.log("Doubled Goal:", doubled);
